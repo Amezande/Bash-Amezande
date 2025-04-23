a275eea9c6eb588053f1a27c45f3bb76213729a3
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2025-04-22 20:33:16 -0600

    Update 3-. Respaldos.sh

--- Archivos/3-. Respaldos.sh
+++ Archivos/3-. Respaldos.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+exito="\e[1;32m"
+error="\e[1;31m"
 #Nombre que le quiere dar al respaldo
-echo "Ingrese el nombre que le quiere dar a su respaldo: "
+echo -e "Ingrese el nombre que le quiere dar a su respaldo: "
 read a
 b="/ruta/a/su/carpeta/o/archivo"
 
@@ -14,8 +16,9 @@ ctrl_c () {
 
 main(){
     echo "Respalde sus archivos."
-
     tar -cvf $a $b/*.*
+
+    if[$? = 0]
 }
 
 main
