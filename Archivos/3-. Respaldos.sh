c5e3c9a5f8dfc7fee99f3f277f546998a8f5ec2e
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2025-04-26 08:45:13 -0600

    Update 3-. Respaldos.sh
    Crea respaldos en .tar.gz

--- Archivos/3-. Respaldos.sh
+++ Archivos/3-. Respaldos.sh
@@ -1,40 +1,15 @@
 #!/bin/bash
 
-exito="\e[1;32m"
-error="\e[1;31m"
-#Nombre que le quiere dar al respaldo
-echo -e "Ingrese el nombre que le quiere dar a su respaldo: "
+verde="\e[1;32m"
+rojo="\e[1;31m"
+
+echo -e "${verde} Ingrese el nombre que le quiere dar a su respaldo: "
 read a
-d=$(pwd)
-b=$(find . -type d | fzf)
-
-trap ctrl_c INT
-
-ctrl_c () {
-    echo -e "\nSaliendo"
-    exit 1
-}
-
-function h {
-    echo "Help: Use el programa para hacer un respaldo de las carpetas que desee, decida donde guardan y más con eestos parámetros"
-}
-
-#Si no se puede realizar el respaldo
-#Si no hay archivos dentro de esa carpeta para respaldar
+b=$(find /home -type d | fzf)
 
 main(){
     echo "Respalde sus archivos."
-    tar -czvf $a.tar.gz $b/*.* 2>/dev/null
-    #CZVF hace tar.gz verbose
-    #Mandando los errores al dev/null
-
-    if [ $? -eq 0 ]
-    then 
-        echo "Archivos respaldados correctamente"
-    else
-
-        echo "Hubo un error en su respaldo"
-    fi
+    tar -czvf $a.tar.gz $b/*.* 2>/dev/null && echo -e "${rojo} Archivos respaldados correctamente"
 }
 
 main
